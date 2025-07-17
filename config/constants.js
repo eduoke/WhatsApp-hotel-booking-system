d7701a7cd4aabfc639b8bf72ccc30f2574c5f663
eduoke <edu_oke@proton.me>
2025-07-17 18:39:28 +0300

    Added a system wide global constants

---
+++ config/constants.js
@@ -0,0 +1,17 @@
+// Global Constants
+// This file defines fixed values used throughout the application,
+// such as conversation states for the bot.
+
+const STATES = {
+    WELCOME: 'welcome',
+    BROWSE_HOTELS: 'browse_hotels',
+    SELECT_HOTEL: 'select_hotel',
+    SELECT_DATES: 'select_dates',
+    CONFIRM_BOOKING: 'confirm_booking',
+    PAYMENT: 'payment',
+    COMPLETED: 'completed'
+};
+
+module.exports = {
+    STATES
+};
