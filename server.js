e90873a78cc52ba2b7052ff6ca63ad23e8bc6b39
eduoke <edu_oke@proton.me>
2025-07-17 16:37:49 +0300

    Remove refactored files

--- server.js
+++
@@ -1,9 +0,0 @@
-import { Client } from 'pg';
-const express = require('express');
-const axios = require('axios');
-const crypto = require('crypto');
-
-const app = express();
-app.use(express.json());
-
-export default app;
